1494872e06d887d449ae2b56e2770f18ebfc3c29
jk705517 <1726603190@qq.com>
2025-12-30 12:58:42 +0800

    平全屏示

--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1668,6 +1668,23 @@ const VideoDetail = () => {
                                             )}
                                         </button>
 
+                                        {/* 平板端全屏按钮（768-1279px） */}
+                                        <button
+                                            onClick={handleToggleFullscreen}
+                                            className="hidden md:block xl:hidden text-white p-1.5 hover:bg-white/10 rounded transition-colors"
+                                            title={isFullscreen ? '退出全屏' : '全屏'}
+                                        >
+                                            {isFullscreen ? (
+                                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
+                                                    <path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z" />
+                                                </svg>
+                                            ) : (
+                                                <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
+                                                    <path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z" />
+                                                </svg>
+                                            )}
+                                        </button>
+
                                         {/* 视频循环按钮 - 手机端隐藏 */}
                                         <button
                                             onClick={() => setIsVideoLooping(!isVideoLooping)}
